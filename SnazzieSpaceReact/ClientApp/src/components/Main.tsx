7665a1e1690ad03814a4f052f74fef7699e6d8d4
Snazzie <cooper.a.m@hotmail.com>
2021-04-14 18:25:22 +0100

    adwa

--- SnazzieSpaceReact/ClientApp/src/components/Main.tsx
+++ SnazzieSpaceReact/ClientApp/src/components/Main.tsx
@@ -1,61 +1,26 @@
-import { Layout, Menu } from 'antd';
-import React from 'react';
+import { Layout, Menu } from "antd";
+import React from "react";
 import {
-  MenuUnfoldOutlined,
-  MenuFoldOutlined,
-  UserOutlined,
-  StarFilled,
-} from '@ant-design/icons';
+	MenuUnfoldOutlined,
+	MenuFoldOutlined,
+	UserOutlined,
+	StarFilled,
+} from "@ant-design/icons";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import { Link, useLocation } from "react-router-dom";
 import "./Main.css";
-const { Sider, Content } = Layout;
-export const Main: React.FunctionComponent = props => {
+export const Main: React.FunctionComponent = (props) => {
+	const [collapsed, setCollapsed] = React.useState<boolean>();
 
-  const [collapsed, setCollapsed] = React.useState<boolean>();
+	function toggleNavbar() {
+		setCollapsed(!collapsed);
+	}
+	const location = useLocation();
 
-  function toggleNavbar () {
-    setCollapsed(!collapsed);
-  }
-  const location = useLocation();
-  
-    return (
-      <Router>
-        <Layout>
-        <Sider theme="dark" trigger={null} collapsible collapsed={collapsed} >
-          <span>
-          <div className="logo" ></div>
-          {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-              className: 'trigger',
-              onClick: toggleNavbar,
-            })}
-            </span>
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={[location.pathname === "/" ? "/" : location.pathname + location.hash]}>
-            <Menu.Item key="/" icon={<UserOutlined />}>
-              Home <Link to={"/"}/>
-            </Menu.Item>
-              <Menu.Item key="/Projects" icon={<StarFilled />}>Projects<Link to={"/Projects"} /></Menu.Item>
-
-
-          </Menu>
-        </Sider>
-        <Layout className="site-layout">
-          <Content
-            className="site-layout-background"
-            style={{
-              margin: '24px 16px',
-              padding: 24,
-              minHeight: 280,
-            }}
-          >
-            {props?.children}
-          </Content>
-          
-        </Layout>
-        </Layout>
-     </Router>
-    );
-  
-  }
-
+	return (
+		<Router>
+			{props?.children}
+		</Router>
+	);
+};
--- SnazzieSpaceReact/ClientApp/src/pages/Home.tsx
+++ SnazzieSpaceReact/ClientApp/src/pages/Home.tsx
@@ -6,38 +6,131 @@ import {
   TwitterIcon,
 } from "../resources/svg/SocialIcons";
 import "./home.scss";
+import {
+  ScrollingProvider,
+  useScrollSections,
+  Section,
+  useScrollSection,
+} from 'react-scroll-section';
+import Meta from "antd/lib/card/Meta";
+import { Layout, Menu, Card, PageHeader } from "antd";
+
+import {
+  UserOutlined,
+  StarFilled,
+  DownCircleOutlined
+} from "@ant-design/icons";
+const { Sider, Header, Content } = Layout;
 
 export const Home: React.FunctionComponent = () => {
+  const sections = useScrollSections();
+
+  const homeSection = useScrollSection('home');
+  const projectsSection = useScrollSection('projects');
   return (
     <>
-      <div className="landingPage">
-        <div className="container">
-          <SnazzieLogo />
-          <div className="socialRow">
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/snazzie"
-            >
-              <GithubIcon />{" "}
-            </a>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://linkedin.com/in/cooper-a-m/"
-            >
-              <LinkedInIcon />
-            </a>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://twitter.com/ItsSnazzie"
-            >
-              <TwitterIcon />
-            </a>
+      <ScrollingProvider>
+        <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+
+          >
+            <Menu.Item key="#home" icon={<UserOutlined />} onClick={homeSection.onClick}>
+              Home
+            </Menu.Item>
+            <Menu.Item key="#Projects" icon={<StarFilled />} onClick={projectsSection.onClick}>
+              Projects
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Section id="home">
+          <div className="background"></div>
+          <div className="container">
+            <div>
+              <SnazzieLogo />
+              <div className="socialRow">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://github.com/snazzie"
+                >
+                  <GithubIcon />{" "}
+                </a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://linkedin.com/in/cooper-a-m/"
+                >
+                  <LinkedInIcon />
+                </a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://twitter.com/ItsSnazzie"
+                >
+                  <TwitterIcon />
+                </a>
+              </div>
+            </div>
+            <div style={{ margin: "auto" }}>
+              <a href="#projects" >
+                <DownCircleOutlined />
+              </a>
+            </div>
           </div>
-        </div>
-      </div>
+        </Section>
+
+        <Section id="projects">
+
+          <div className="container">
+            <PageHeader
+              className="site-page-header"
+              title="Projects"
+            />
+            <div className="grid">
+
+              <Card
+                hoverable
+                onClick={() => window.open("https://github.com/darkthemehub", "_blank")}
+                style={{ width: 300 }}
+                cover={
+                  <img
+                    alt="image"
+                    style={{ width: 300, height: "auto", background: "#1d1d1d" }}
+                    src={`https://avatars2.githubusercontent.com/u/55282763?s=400&u=6de6bebec65ca7e8102b96d354a2da3102919da9&v=4`}
+                  />
+                }
+                actions={[]}
+              >
+                <Meta
+                  title="Dark Theme Hub"
+                  description="Dark themes for developers"
+                />
+              </Card>
+              <Card
+                hoverable
+                onClick={() => window.open("https://github.com/Snazzie/Affinity", "_blank")}
+                style={{ width: 300 }}
+                cover={
+                  <img
+                    alt="image"
+                    style={{ width: 300, height: "auto", background: "#1d1d1d" }}
+                    src={`https://i.gyazo.com/5a5abc0e46d96b90602fc1087a9b5863.png`}
+                  />
+                }
+                actions={[]}
+              >
+                <Meta
+                  title="Affinity"
+                  description="Windows 10 Application Affinity (CPU Threads) Manager"
+                />
+              </Card>
+            </div>
+          </div>
+        </Section>
+      </ScrollingProvider>
     </>
   );
 };
