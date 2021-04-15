e255712dd635a1e8b718ad0c306f54d52211baa6
Snazzie <cooper.a.m@hotmail.com>
2021-04-15 11:13:19 +0100

    adsad

--- SnazzieSpaceReact/ClientApp/src/pages/Home.tsx
+++ SnazzieSpaceReact/ClientApp/src/pages/Home.tsx
@@ -89,44 +89,20 @@ export const Home: React.FunctionComponent = () => {
               className="site-page-header"
               title="Projects"
             />
-            <div className="grid">
-
-              <Card
-                hoverable
+            <div className="cards">
+              <ProjectCard
                 onClick={() => window.open("https://github.com/darkthemehub", "_blank")}
-                style={{ width: 300 }}
-                cover={
-                  <img
-                    alt="image"
-                    style={{ width: 300, height: "auto", background: "#1d1d1d" }}
-                    src={`https://avatars2.githubusercontent.com/u/55282763?s=400&u=6de6bebec65ca7e8102b96d354a2da3102919da9&v=4`}
-                  />
-                }
-                actions={[]}
-              >
-                <Meta
-                  title="Dark Theme Hub"
-                  description="Dark themes for developers"
-                />
-              </Card>
-              <Card
-                hoverable
+                imgSource={`https://avatars2.githubusercontent.com/u/55282763?s=400&u=6de6bebec65ca7e8102b96d354a2da3102919da9&v=4`}
+                title="Dark Theme Hub"
+                description="Dark themes for developers"
+              />
+              <ProjectCard
                 onClick={() => window.open("https://github.com/Snazzie/Affinity", "_blank")}
-                style={{ width: 300 }}
-                cover={
-                  <img
-                    alt="image"
-                    style={{ width: 300, height: "auto", background: "#1d1d1d" }}
-                    src={`https://i.gyazo.com/5a5abc0e46d96b90602fc1087a9b5863.png`}
-                  />
-                }
-                actions={[]}
-              >
-                <Meta
-                  title="Affinity"
-                  description="Windows 10 Application Affinity (CPU Threads) Manager"
-                />
-              </Card>
+                imgSource="https://i.gyazo.com/5a5abc0e46d96b90602fc1087a9b5863.png"
+                title="Affinity"
+                description="Windows 10 Application Affinity (CPU Threads) Manager"
+              />
+
             </div>
           </div>
         </Section>
@@ -134,3 +110,26 @@ export const Home: React.FunctionComponent = () => {
     </>
   );
 };
+
+interface ProjectCardProps {
+  onClick?: () => void;
+  imgSource: string;
+  title: string;
+  description: string;
+}
+
+function ProjectCard(props: ProjectCardProps) {
+
+  return (
+    <div className="card"
+      onClick={props.onClick}>
+      <img
+        alt="image"
+        style={{ width: "100%", height: "auto", background: "#1d1d1d" }}
+        src={props.imgSource} />
+      <div className="detail">
+        <h4>{props.title}</h4>
+        <div>{props.description}</div>
+      </div>
+    </div>)
+}
