65ea534c0d931416d0a7bdb5bda42b7ec43ca68b
Aaron <cooper.a.m@hotmail.com>
2023-01-27 19:22:01 +0000

    firebase analytics

--- Website/src/main.tsx
+++ Website/src/main.tsx
@@ -3,6 +3,30 @@ import App from './App'
 import './custom.css'
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { initializeApp } from "firebase/app";
+
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBDe7uC6_EvpiCb5HcNaNSEi5Z5x2YKojI",
+
+  authDomain: "snazzie-space.firebaseapp.com",
+
+  projectId: "snazzie-space",
+
+  storageBucket: "snazzie-space.appspot.com",
+
+  messagingSenderId: "588360938634",
+
+  appId: "1:588360938634:web:a8a64cd83a93239e76a973",
+
+  measurementId: "G-FDCWB5RXMW"
+
+};
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
+
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(<React.StrictMode>
