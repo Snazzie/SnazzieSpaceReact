3061c17f2e3ba8292eba075e8719e940132ec8fc
Snazzie <cooper.a.m@hotmail.com>
2026-06-02 23:51:02 +0100

    feat(articles): install mdx, rss, typography deps

--- Website/astro.config.mjs
+++ Website/astro.config.mjs
@@ -3,14 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
 
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://snazzie.space",
   base: "/",
-  integrations: [react(), sitemap()],
+  integrations: [react(), mdx(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
